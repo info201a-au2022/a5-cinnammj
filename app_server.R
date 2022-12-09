4ea07977e589d3b99d4268179fc8a0c8335ec193
cinnammj <cinnammj@uw.edu>
2022-12-09 00:20:22 -0800

    final

--- app_server.R
+++ app_server.R
@@ -3,90 +3,30 @@ library("dplyr")
 library("plotly")
 
 #source files
-source("./source/charts/political_party_chart.R")
 
 # CSV files
-hate_crimes <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-3-section-ah/main/data/hate_crime.csv") %>%
-  rename(year = DATA_YEAR)
-state_codes <- read.csv('https://raw.githubusercontent.com/info201a-au2022/project-group-3-section-ah/main/data/state_codes.csv', stringsAsFactors = FALSE)
-df1 <- read.csv('https://raw.githubusercontent.com/info201a-au2022/project-group-3-section-ah/main/data/Hate_crime_laws_by_state.csv', stringsAsFactors = FALSE)
-
-#Michael's Function for Map
-build_map <- function(data, map.var) {
-  
-  l <- list(color = toRGB("white"), width = 2)
-  
-  g <- list(
-    scope = 'usa',
-    projection = list(type = 'albers usa'),
-    showlakes = TRUE,
-    lakecolor = toRGB('white')
-  )
-  
-  var.equation <- paste0('~', map.var)
-  
-  p <- plot_geo(data, locationmode = 'USA-states') %>%
-    add_trace(
-      z = data[,map.var], text = ~state, locations = ~code,
-      color = data[,map.var], colors = 'Purples'
-    ) %>%
-    colorbar(title = "Color Title") %>%
-    layout(
-      title = str_to_title(map.var),
-      geo = g
-    )
-  return(p)
-}
-
+co2data <- read.csv("https://raw.githubusercontent.com/info201a-au2022/a5-cinnammj/main/owid-co2-data.csv", stringsAsFactors = FALSE) #%>% filter(iso_code == "USA")
+RecentChina <- filter(co2data, year == max(year)) %>% filter(country == "China") %>% select(co2)
+RecentUS <- filter(co2data, year == max(year)) %>% filter(country == "United States") %>% select(co2)
+RecentIndia <- filter(co2data, year == max(year)) %>% filter(country == "India") %>% select(co2)
+RecentWorld <- filter(co2data, year == max(year)) %>% filter(country == "World") %>% select(co2)
+PercentUS <- (RecentUS$co2/RecentWorld$co2) * 100
+PercentChina <- (RecentChina$co2/RecentWorld$co2) * 100
+PercentIndia <- (RecentIndia$co2/RecentWorld$co2) * 100
 
 # Server  
-server <- function(input, output) {
   
-  # Qiqi's plot
-  output$motovation_plot <- renderPlotly({
-    
-    plot_data <- hate_crimes %>% 
-      filter(year == input$yearvar) %>%
-      group_by(BIAS_DESC) %>%
-      summarise(num_biases = n()) %>%
-      mutate(biases_array = strsplit(BIAS_DESC, ";")) %>%
-      unnest(biases_array) %>%
-      group_by(biases_array)%>%
-      summarise(new_num_biases = n()) %>%
-      mutate(proportion = (new_num_biases/sum(new_num_biases))*100) %>%
-      mutate(year = input$yearvar)
-    
-    plot <- plot_ly(data = plot_data, x = ~biases_array, y = ~proportion, type = 'bar') %>%
-      layout(xaxis = list(autotypenumbers = 'strict', title = 'Types of biases'),
-             yaxis = list(title = 'Proportion'),
-             plot_bgcolor='#e5ecf6',
-             xaxis = list(
-               zerolinecolor = '#ffff',
-               zerolinewidth = 2,
-               gridcolor = 'ffff'),
-             yaxis = list(
-               zerolinecolor = '#ffff',
-               zerolinewidth = 2,
-               gridcolor = 'ffff'))
-    
-  })
-  
-  
-  # Jiyoon's plot
-  output$political_chart <- renderPlotly({
-    
-    plot_ly(party_crimerates, x = ~state, y = ~crime_ratio) %>%
-      filter(party == input$party) %>%
-      group_by(party) %>%
-      add_markers()
-    
-  })
-  
-  # Michael's plot
-  output$map <- renderPlotly({ 
-    
-    return(build_map(df1, input$mapvar))
-  }) 
-  
-}
+countryco2 <- co2data[, c(1, 2, 3, 4, 8)] 
 
+server <- function(input, output, session) 
+{
+  
+  overallco2 <- reactive({
+    countryco2 %>% filter(iso_code %in% input$country, year %in% c(input$time[1]:input$time[2]))
+  })
+  
+  output$co2emissions <- renderPlotly({ggplotly(ggplot(overallco2(), aes(x= year, y= co2, color = iso_code)) + 
+                                                     geom_line() + ggtitle("CO2 emissions by year") + 
+                                                  labs(y = "CO2 emissions measured in million tonnes"))                                                
+  })
+}
--- app_ui.R
+++ app_ui.R
@@ -3,102 +3,57 @@ library("shiny")
 library("dplyr")
 library("plotly")
 library(shinythemes)
-
-
-
-co2 <- read.csv("https://raw.githubusercontent.com/info201a-au2022/project-group-3-section-ah/main/data/hate_crime.csv") %>%
-  rename(year = DATA_YEAR)
-
+source("app_server.R")
 
 introduction_page <- tabPanel(
   "Introduction",
-  img(src = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJBiNfSUrPPb2EnwfP81aT6PibwLHYAaw9ag&usqp=CAU", height = 250, width = "100%"),
-  h2("Introduction"),
-  p("Our project focuses on analyzing trends in hate crimes. The questions we are aiming to address include: What are the causes and motivations behind hate crimes? How are hate crimes punishable across different states? How does the political party affiliation by state correspond with crime rates? The first dataset we will be using comes from the FBI's Uniform Crime Reporting (UCR) Program. This dataset includes various information about the hate crime, such as demographics of vistoms/perpetrators and potential causes behind that hate crime data. The second dataset we will analyze comes Harvard Dataverse. This dataset tells us the voting outcome for presidential candidates from years 1976-2020 and can potentially give insight on how political party affiliation may be linked with hate crimes. The third dataset is the US Population dataset from Kaggle. This dataset tells us the population in each state in the U.S. in 2020. Our final dataset is from ADL(Anti-Defamation League). This dataset tells us which states have laws protecting hate crimes, and if not all then which hate crimes it protects.")
+  img(src = "https://atlas-scientific.com/files/CO2-blog-img-122221.jpg", height = 250, width = "100%"),
+  h1(strong("CO2 Emissions")),
+  h3("Overview"),
+  p("CO2 emissions have been a huge factor in todays day and age, but who's making the most CO2 emissions? When looking at the data theres no surprise that China, The US, and India are all big factors in CO2 emissions. With that said, two questions I will answer is how much CO2 is each county putting out in 2022, and what percentage of the world CO2 is it in 2022. My graph will have every country that has released CO2, so a lot, and there CO2 trend over many many years."),
+  h3("World"),
+  p("The world as a whole have released this much CO2 in 2022"),
+  p(RecentWorld, "CO2 in million tonnes"),
+  h3("United States"),
+  p("The United States has released this much CO2 in 2022"), 
+  p(RecentUS, "CO2 in million tonnes"),
+  p("With the percentage of its part in world CO2 emission being"), 
+  p(PercentUS, "%"),
+  h3("China"),
+  p("China has released this much CO2 in 2022"),
+  p(RecentChina, "CO2 in million tonnes"),
+  p("With the percentage of its part in world CO2 emission being"), 
+  p(PercentChina, "%"),
+  h3("India"),
+  p("India has released this much CO2 in 2022"), 
+  p(RecentIndia, "CO2 in million tonnes"),
+  p("With the percentage of its part in world CO2 emission being"),
+  p(PercentIndia, "%"),
+
 )
 
-# Sidebar
-inputs <- sidebarPanel(
+page2 <- tabPanel(
+  "Carbone Dioxide Emissions", 
+  h1(strong("CO2 Emissions by Country")),
+  p("Select country(s) and timeframe to see the CO2 Emissions"),
+  img(), 
   
-  selectInput(
-    inputId = "yearvar",
-    label = "Choose year",
-    choices = c(2010,2011,2012,2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020)
-  )
-)
-
-# Interactive Page1
-
-interactive_page <- tabPanel(
-  "Bar plot",
-  sidebarLayout(inputs,
-                mainPanel(plotlyOutput("motovation_plot"))),
-  p(em("This bar plot showcases the proportion (%) of the causes and motivations behind hate crimes for a specified year (2000-2010). Across the decade, anti racial sentiments seem to be the main motivators behind hate crimes."))
-)
-
-
-# Interactive page 2 - Michael
-Takeaways <- tabPanel(
-  "Key Takeaways",
-  h2("The Three Key Takeaways"),
-  p("1.) we found out that the political party affiliation of each state did not necessarily correspond with lower hate crime rates. States that affiliate with the Democratic party had higher hate crime rates, with the District of Columbia, New Jersey, and Vermont being the biggest outliers. On the contrary, states that affiliate with the Republican party had lower hate crime rates in general."),
-  
-  p("2.) Most states have punishments for Hate crimes, excluding Arkansas, Indiana, South Carolina, and Wyoming. and although most states have laws protecting things such as race, religion, and ethnicity. Some states start to dwindle on Disability, Gender, Sexual Orientation, and most of all Gender Identity."), 
-  
-  p("3.) The top 3 Hate crimes (documented) that were committed were Anti-Black or African American, Anti-Gay (Male), and Anti-Jewish. Which means that Racism, Anti-Semitism, and Homophobia are still prevalent, however that doesn't mean only those things exist, overall hatred is too big of a problem in our country")
-)
-
-
-map_sidebar_content <- sidebarPanel(selectInput("mapvar", label = "Variable to Map", 
-                                                choices = list("Race, Religion, and Ethnicity" = "Race.Religion.Ethnicity","Disability" = "Disability"
-                                                               ,"Gender" = "Gender", "Sexual Orientation" = "Sexual.Orientation", "Gender Identity" = "Gender.Identity")))
-
-map_main_content <- mainPanel(plotlyOutput("map"))
-
-map_panel <- tabPanel("Map",titlePanel("Protection by Identifier"), sidebarLayout(map_sidebar_content, map_main_content),
-                      p(em("This map shows what certain subgroups of people (ex. race, gender, disability) are protected by laws in each state, the laws protect these groups from hate crimes being committed agaisnt them and can also further the persons punishment if they commit a hate crime against these people in the states they are protected in.")))
-
-
-# Interactive page 3 - political party (Jiyoon)
-interactive_political_party <- tabPanel(
-  "Political Party",
-  titlePanel("Political Party"),
   sidebarLayout(
     sidebarPanel(
-      selectizeInput(inputId = "party",
-                  label = "Political Party Identification",
-                  choices = list("Republican" = "Republican", "Democrat" = "Democrat"),
-                  selected = "Democrat",
-                  multiple = FALSE
-      )
-    ),
-    mainPanel(
-      h3("Political Party Comparison"),
-      p(plotlyOutput(outputId = "political_chart"),
-      p("This plot shows the states of the U.S., identifies the political party affiliation of each state, and shows their hate crime rates in percentages (calculated in ratio: hate crime rates divided by total population of each state) in 2020.")
-      )
-    )
-  )
-)
+      selectizeInput(
+        inputId = "country", 
+        label = "Select Country(s)", 
+        choices = c("ZWE","ZAF","YEM","WSM","VNM","VIR","VEN","VCT","UZB","USA","URY","UKR","UGA","TZA","TWN","TUV","TUN","TTO","TON","TLS","TKM","THA","TCA","SYR","SXM","SWZ","SWE","SVK","SUR","SSD","SRB","SPM","SOM","SLV","SLE","SLB","SJM","SGP","EN","SAU","RWA","RUS","REU","QAT","PSE","PRY","PRT","PRK","PRI","POL","PNG","PER","PAN","PAK","NZL","NPL","NOR","NGA","NER","NCL","NAM","MYT","MYS","MUS","MTQ","MSR","MRT","MOZ","MNG","MNE","MMR","MLT","MLI","MEX","MDG","LVA","LUX","LTU","LSO","LCA","LBY","LBR","LAO","KOR","KNA","KIR","KHM","KGZ","KAZ","JOR","JEY","JAM","ITA","ISR","ISL","IRQ","IRL","IND","HUN","HTI","HRV","HND","HKG","GUF","GTM","GRD","GRC","GNQ","GNB","GMB","GLP","GIN","GHA","GGY","GBR","GAB","FRA","FLK","FJI","FIN","ETH","EST","ESP","ESH","EGY","DOM","DMA","DJI","DEU","CZE","CYP","CUB","CRI","CPV","COL","COG","COD","CIV","CHN","CHL","CAN","CAF","BWA","BRN","BRB","BRA","BOL","BMU","BLZ","BLR","BHS","BHR","BGR","BFA","BES","BEL","BDI","AUT","AUS","ATG","ASM","ARM","ARE","ANT","AND","ALB","ALA","AIA","AGO","AFG"), 
+        selected = c("GBR", "CHN", "USA", "IND"),
+        multiple = TRUE
+      ),
+      
+      sliderInput("time", "Select timeframe", min = 1750, max = 2021, value = c(1750, 2021), sep = "")),
+    
+    mainPanel(plotlyOutput("co2emissions"), 
+              p("This chart shows the CO2 Emissions of the selected countries from the earliest date recored(differs in each country) to the most recent date, being 2021. You can learn which countries have released the most CO2, along with the trend (increasing or decreasing) of CO2 emission in each country"))))
 
-# Report page - Jiyoon
-report <- tabPanel(
-  "Report",
-  titlePanel("Report"),
-  mainPanel(
-  includeMarkdown("./docs/p01-proposal.md")
-  )
-)
+ui <- navbarPage("CO2 Emissions", theme = shinytheme("darkly"), introduction_page, page2)
 
-
-ui <- navbarPage("Hate Crimes in the U.S.",
-  theme = shinytheme("darkly"),
-  introduction_page, # qiqi's intro page 
-  interactive_page, #qiqi's interactive page
-  map_panel, #michael's interactive page goes here
-  interactive_political_party, #Jiyoon's interactive page
-  Takeaways, #michael's takeaways
-  report #report file by Jiyoon
-)
 
 
